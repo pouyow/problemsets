07e83221a68aa29a3bff69352949e402c588418a
pouyow <142265774+pouyow@users.noreply.github.com>
2023-08-17 09:47:57 +0000

    Thu, Aug 17, 2023, 2:17 PM +04:30

--- deep/deep.py
+++ deep/deep.py
@@ -1,5 +1,5 @@
 inputt = input("enter: ").strip().lower()
-if inputt != ("42") and inputt != ("forty two") and inputt != ("forty_two"):
+if inputt != ("42") and inputt != ("forty two") and inputt != ("forty-two"):
     print ("No")
 else:
     print ("Yes")
