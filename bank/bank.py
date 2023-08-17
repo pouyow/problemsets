bcf7dcd934c27f8d0a1a05ea7eead30f36773d16
pouyow <142265774+pouyow@users.noreply.github.com>
2023-08-17 13:32:04 +0000

    Thu, Aug 17, 2023, 6:02 PM +04:30

--- bank/bank.py
+++ bank/bank.py
@@ -1,6 +1,6 @@
 koli = input("entet: ")
 flitt = koli.capitalize()[0].lower().strip()
-fword = koli.split()[0].lower().strip
+fword = koli.split()[0].lower().strip()
 if fword == ("hello") and fword != flitt:
     print ("$0")
 elif flitt == ("h"):
