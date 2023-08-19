e3149457b96125d0bbca2c533f8da26e667c34a2
pouyow <142265774+pouyow@users.noreply.github.com>
2023-08-19 14:23:58 +0000

    Sat, Aug 19, 2023, 6:53 PM +04:30

--- meal/meal.py
+++ meal/meal.py
@@ -1,15 +1,15 @@
 def main():
     x = input("enter: ")
-    cnvrt (x)
-
-def cnvrt(time):
+    y = convert (x)
+    if (7>=y<=8):
+        print("breakfast time")
+    elif (12>=y<=13):
+        print ("lunch time")
+    elif (18>=y<=19):
+        print ("dinner time")
+def convert(time):
     tim = time.split(":")
     hour = int(tim[0])
-    minute = int(tim[1])
-    if (7>=hour<=8):
-        print("breakfast time")
-    elif (12>=hour<=13):
-        print ("lunch time")
-    elif (18>=hour<=19):
-        print ("dinner time")
+    minute = int(tim[1]) / 100
+    return (hour + minute)
 main()
