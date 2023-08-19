a1e1e2cfafd63d4d5a5be5e279497034ee24ca90
pouyow <142265774+pouyow@users.noreply.github.com>
2023-08-19 15:00:58 +0000

    Sat, Aug 19, 2023, 7:30 PM +04:30

--- meal/meal.py
+++ meal/meal.py
@@ -1,18 +1,18 @@
-
-def convert(time):
-    tim = time.split(":")
-    hour = int(tim[0])
-    minute = int(tim[1]) / 100
-    return (hour + minute)
-
 def main():
     x = input("enter: ")
     y = convert (x)
-    if (7>=y<=8):
+    print (y)
+    if y >= 7 and y <= 8:
         print("breakfast time")
-    elif (12>=y<=13):
+    elif (12 <= y >= 13):
         print ("lunch time")
-    elif (18>=y<=19):
+    elif (18 >= y <= 19):
         print ("dinner time")
-
-main()
+def convert(time):
+    #or hour , minute = time.split(":")
+    tim = time.split(":")
+    hour = float(tim[0])
+    minute = float(tim[1]) / 60
+    return (hour +(minute))
+if __name__ == "__main__":
+    main()
