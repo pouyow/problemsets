16a663598d0b3ec9836bf21acf85cf00102d875e
pouyow <142265774+pouyow@users.noreply.github.com>
2023-08-22 22:09:00 +0000

    Wed, Aug 23, 2023, 2:39 AM +04:30

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -2,16 +2,16 @@ def main():
     payam = input("enter: ")
     q = ["e","a","i","u","o"]
     f=find(payam, q)
-    ss= ''.join(f)
-    s = payam.replace(ss,"")
-    print(s)
-
+    for i in f:
+         payam = payam.replace(i,"")
+    print(payam)
 
 def find(payam, q):
      result=[]
      for i in payam:
           if i in q:
-              result.append(i)
-              return result
+           result.append(i)
+     return result
+
 
 main()
