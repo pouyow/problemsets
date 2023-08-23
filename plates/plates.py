0b4c321372ab49476c14c5dd8a86b647c43ebf44
pouyow <142265774+pouyow@users.noreply.github.com>
2023-08-23 16:20:11 +0000

    Wed, Aug 23, 2023, 8:50 PM +04:30

--- plates/plates.py
+++ plates/plates.py
@@ -6,7 +6,7 @@ def main():
       print("Invalid")
 def is_valid(s):
      while True:
-      if len(s) >= 2 and len(s) <= 6 and s[:2].isalpha() and s[:2].isupper() and s[2:].isdigit() and "." not in s and " " not in s and "?" not in s and "!" not in s and "0" not in len(s[0]):
+      if len(s) >= 2 and len(s) <= 6 and s[:2].isalpha() and s[:2].isupper() and s[2:].isdigit() and "." not in s and " " not in s and "?" not in s and "!" not in s and "0" not in s[0]:
        return True
       else:
         return False
