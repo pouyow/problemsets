7005dcffecf82f9984fb33a51dc402919f9ead28
pouyow <142265774+pouyow@users.noreply.github.com>
2023-08-23 14:32:12 +0000

    Wed, Aug 23, 2023, 7:02 PM +04:30

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,7 +1,7 @@
 payam = input("enter: ")
 print("output:" , end="")
 for letter in payam:
-    if not letter in ["e","a","i","u","o"]:
+    if not letter.lower in ["e","a","i","u","o"]:
       print(letter, end="")
 
 print()
