2c81790cb7f5cfe05c03b6bc1f8dd282606ddba3
pouyow <142265774+pouyow@users.noreply.github.com>
2023-08-24 01:17:07 +0000

    Thu, Aug 24, 2023, 5:47 AM +04:30

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,25 +1,29 @@
-d = {"Apple" : "130" ,
-"Avocado" : "50" ,
-"Banana" : "110" ,
-"Cantaloupe" : "50" ,
-"Grapefruit" : "60" ,
-"Grapes" : "90" ,
-"Honeydew Melon" : "50" ,
-"Kiwifruit" : "90" ,
-"Lemon" : "15" ,
-"Lime" : "20" ,
-"Nectarine" : "60" ,
-"Orange" : "80" ,
-"Peach" : "60" ,
-"Pear" : "100" ,
-"PineApple" : "50" ,
-"Plums" : "70",
-"Strawberries" : "50" ,
-"SweetCherries" : "100" ,
-"Tangerine" : "50" ,
-"Watermelon" : "80"}
-k = input("enter: ").lower
+d = {
+    "Apple": "130",
+    "Avocado": "50",
+    "Banana": "110",
+    "Cantaloupe": "50",
+    "Grapefruit": "60",
+    "Grapes": "90",
+    "Honeydew Melon": "50",
+    "Kiwifruit": "90",
+    "Lemon": "15",
+    "Lime": "20",
+    "Nectarine": "60",
+    "Orange": "80",
+    "Peach": "60",
+    "Pear": "100",
+    "Pineapple": "50",
+    "Plums": "70",
+    "Strawberries": "50",
+    "Sweet Cherries": "100",
+    "Tangerine": "50",
+    "Watermelon": "80"
+}
+
+k = input("enter: ").lower()
+
 for s in d:
-    if k in s.lower():
-     print(s , d[s])
-    break
+    if k.lower() == s.lower():
+        print(s, d[s])
+        break
