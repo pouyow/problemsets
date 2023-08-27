1cd7870cb7d73f0faed7a0bca4eb4a2c33153c74
pouyow <142265774+pouyow@users.noreply.github.com>
2023-08-27 21:23:26 +0000

    Mon, Aug 28, 2023, 1:53 AM +04:30

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -13,7 +13,7 @@ def getxy(x):
      ff = input("enter: ").split("/")
      x = int(ff[0])
      y = int(ff[1])
-     q = float(round(x/y *100))
+     q = round(float(x/y *100))
      return q
 
     except (ValueError, ZeroDivisionError):
