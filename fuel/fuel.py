fbefb16c99069abab7ce443d758e94139bc975dc
pouyow <142265774+pouyow@users.noreply.github.com>
2023-08-27 21:02:01 +0000

    Mon, Aug 28, 2023, 1:32 AM +04:30

---
+++ fuel/fuel.py
@@ -0,0 +1,21 @@
+def main():
+   x = getxy("enter fuel range x/y: ")
+   print(f"{x}%")
+
+def getxy(x):
+ while True:
+    try:
+     ff = input("enter: ").split("/")
+     x = int(ff[0])
+     y = int(ff[1])
+     q = (x/y)
+     if q == 0:
+        return ("E")
+     elif q == 1:
+        return ("F")
+     else:
+        return q
+
+    except (ValueError, ZeroDivisionError):
+       pass
+main()
