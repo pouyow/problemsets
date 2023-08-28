cbbbd26655bcf76b609e317ddbc41b009bc32800
pouyow <142265774+pouyow@users.noreply.github.com>
2023-08-28 08:16:13 +0000

    Mon, Aug 28, 2023, 12:46 PM +04:30

---
+++ taqueria/taqueria.py
@@ -0,0 +1,22 @@
+nar = [{
+"Baja Taco": 4.00,
+"Burrito": 7.50,
+"Bowl": 8.50,
+"Nachos": 11.00,
+"Quesadilla": 8.50,
+"Super Burrito": 8.50,
+"Super Quesadilla": 9.50,
+"Taco": 3.00,
+"Tortilla Salad": 8.00
+}]
+
+while True:
+    try:
+        inp = input("Item: ").title()
+        for i in nar:
+            print(i[inp])
+    except(KeyError):
+        pass
+    except(EOFError):
+        exit()
+
