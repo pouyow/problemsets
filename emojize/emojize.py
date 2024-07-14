473bea15dbe6d1c07b4f44bb01d11dcb2c3252cb
pouyow <142265774+pouyow@users.noreply.github.com>
2024-07-14 18:40:38 +0000

    Sun, Jul 14, 2024, 10:10 PM +03:30

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,10 +1,5 @@
 import emoji
 
-# Read input from the user
 x = input().strip()
-
-# Convert the input string to emojis with aliases enabled
 s = emoji.emojize(x, language='alias')
-
-# Print the resulting string
 print(s)
