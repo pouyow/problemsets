33a73ca4a19847adf53b8b0b98e8d5bed8738c24
pouyow <142265774+pouyow@users.noreply.github.com>
2024-07-16 21:30:34 +0000

    Wed, Jul 17, 2024, 1:00 AM +03:30

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,18 +1,15 @@
 import json
 import requests
 import sys
-try:
-    if len(sys.argv) != 2:
-        sys.exit(1)
-    if not (sys.argv).isdigit:
-        sys.exit(1)
-    n = sys.argv[1]
-    int_n = float(n)
-    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-    response_js = response.json()
-    rate = response_js.get("bpi", {}).get("USD", {}).get("rate")
-    integer = (rate).replace(",", "")
-    amount = (float(integer) * int_n)
-    print(f"${amount:,.4f}")
-except ValueError:
-    print("fuck u")
+n = sys.argv[1]
+if len(sys.argv) != 2:
+   sys.exit(1)
+if not (n).isdigit():
+    sys.exit(1)
+int_n = float(n)
+response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+response_js = response.json()
+rate = response_js.get("bpi", {}).get("USD", {}).get("rate")
+integer = (rate).replace(",", "")
+amount = (float(integer) * int_n)
+print(f"${amount:,.4f}")
