1cd7180a80db25182952c63ca5ea77b02ab75b03
pouyow <142265774+pouyow@users.noreply.github.com>
2024-07-21 21:35:54 +0000

    Mon, Jul 22, 2024, 1:05 AM +03:30

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -5,7 +5,7 @@ def test_hello():
     assert value("hello") == 0
     assert value("Hello") == 0
     assert value("HELLO") == 0
-    assert value("hello, world") == 0
+    assert value("hello,ooo") == 0
 
 def test_h():
     assert value("hi") == 20
@@ -14,10 +14,10 @@ def test_h():
     assert value("hey") == 20
 
 def test_other():
-    assert value("goodbye") == 100
-    assert value("Goodbye") == 100
-    assert value("GOODBYE") == 100
-    assert value("what's up?") == 100
+    assert value("sssss") == 100
+    assert value("LALALALA") == 100
+    assert value("GOOZ") == 100
+    assert value("sex?") == 100
 
 if __name__ == "__main__":
     pytest.main()
