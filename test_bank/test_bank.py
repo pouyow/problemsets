07b560b963932da14d5ae2ccb0301f5df2ae8302
pouyow <142265774+pouyow@users.noreply.github.com>
2024-07-21 21:34:15 +0000

    Mon, Jul 22, 2024, 1:04 AM +03:30

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,19 +1,22 @@
 import pytest
 from bank import value
 
-import pytest
-from bank import value
-
 def test_hello():
     assert value("hello") == 0
+    assert value("Hello") == 0
+    assert value("HELLO") == 0
     assert value("hello, world") == 0
 
 def test_h():
     assert value("hi") == 20
+    assert value("Hi") == 20
+    assert value("HI") == 20
     assert value("hey") == 20
 
 def test_other():
     assert value("goodbye") == 100
+    assert value("Goodbye") == 100
+    assert value("GOODBYE") == 100
     assert value("what's up?") == 100
 
 if __name__ == "__main__":
