ba86872b9714ea2ca7073b9f897252379e99a2e1
pouyow <142265774+pouyow@users.noreply.github.com>
2024-07-21 21:28:32 +0000

    Mon, Jul 22, 2024, 12:58 AM +03:30

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -5,12 +5,12 @@ def main():
 
 def value(greeting):
     greeting = greeting.lower().strip()
-    if greeting == "hello":
-        return "$0"
+    if greeting.startswith("hello"):
+        return 0
     elif greeting.startswith("h"):
-        return "$20"
+        return 20
     else:
-        return "$100"
+        return 100
 
 if __name__ == "__main__":
     main()
--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,12 +1,20 @@
 import pytest
 from bank import value
 
-def test_value():
-    assert value("hello") == "$0"
-    assert value("h") == "$20"
-    assert value("hey") == "$20"
-    assert value("goodbye") == "$100"
-    assert value("anything else") == "$100"
+import pytest
+from bank import value
+
+def test_hello():
+    assert value("hello") == 0
+    assert value("hello, world") == 0
+
+def test_h():
+    assert value("hi") == 20
+    assert value("hey") == 20
+
+def test_other():
+    assert value("goodbye") == 100
+    assert value("what's up?") == 100
 
 if __name__ == "__main__":
     pytest.main()
