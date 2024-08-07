9f61d4c3b236c65796524b226353d175d08d757a
pouyow <142265774+pouyow@users.noreply.github.com>
2024-08-07 17:17:51 +0000

    Wed, Aug 7, 2024, 8:47 PM +03:30

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from PIL import Image, ImageOps
 
 def main():
@@ -14,7 +15,10 @@ def main():
     if not (input_image.lower().endswith(('.png', '.jpg', '.jpeg')) and output_image.lower().endswith(('.png', '.jpg', '.jpeg'))):
         sys.exit("Invalid output")
 
-    if input_image.split('.')[-1] != output_image.split('.')[-1]:
+    input_ext = os.path.splitext(input_image)[1]
+    output_ext = os.path.splitext(output_image)[1]
+
+    if input_ext.lower() != output_ext.lower():
         sys.exit("Input and output have different extensions")
 
     try:
@@ -23,8 +27,9 @@ def main():
         sys.exit("Input does not exist")
 
     shirt = Image.open("shirt.png")
+    size = shirt.size
 
-    image = ImageOps.fit(image, shirt.size, method=Image.Resampling.LANCZOS)
+    image = ImageOps.fit(image, size, method=Image.Resampling.LANCZOS)
 
     image.paste(shirt, (0, 0), shirt)
 
