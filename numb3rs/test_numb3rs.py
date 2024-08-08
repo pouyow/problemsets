a0e2e683513fb9694f95cd948b3672bcef4d2b95
pouyow <142265774+pouyow@users.noreply.github.com>
2024-08-08 21:05:33 +0000

    Fri, Aug 9, 2024, 12:35 AM +03:30

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -11,13 +11,14 @@ def test_invalid():
     assert validate("0.0.0.0.0") == False
     assert validate("10.10.10") == False
     assert validate("2001:0db8:85a3:0000:0000:8a2e:0370:7334") == False
-
+    assert validate("2.2.2") == False
+    assert validate("500.12.22.14") == False
 def test_valid():
     assert validate("0.0.0.0") == True
     assert validate("255.255.255.255") == True
     assert validate("127.0.0.1") == True
     assert validate("192.168.1.1") == True
-    assert validate("2.2.2") == False
+
 
 if __name__ == "__main__":
     main()
