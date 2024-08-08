7fa5f8305617e6c7b930fb08c959a2fd53af982c
pouyow <142265774+pouyow@users.noreply.github.com>
2024-08-08 23:25:19 +0000

    Fri, Aug 9, 2024, 2:55 AM +03:30

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -6,7 +6,7 @@ def main():
 def validate(ip):
     pattern = r"^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}$"
 
-    if re.fullmatch(pattern, ip):
+    if re.search(pattern, ip):
         return True
     return False
 
