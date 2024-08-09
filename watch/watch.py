9511fb04100dab579628fd9cf2b78c45671b1d4b
pouyow <142265774+pouyow@users.noreply.github.com>
2024-08-09 08:20:01 +0000

    Fri, Aug 9, 2024, 11:50 AM +03:30

--- watch/watch.py
+++ watch/watch.py
@@ -5,9 +5,9 @@ def main():
     print(parse(html))
 
 def parse(s):
-    match = re.search(r'<iframe [^>]*src="(https?://(?:www\.)?youtube\.com/embed/[^"]+|http://(?:www\.)?youtube\.com/embed/[^"]+)"', s)
+    matches = re.search(r'<iframe [^>]*src="(https?://(?:www\.)?youtube\.com/embed/[^"]+|http://(?:www\.)?youtube\.com/embed/[^"]+)"', s)
     if match:
-        url = match.group(1)
+        url = matches.group(1)
         url = re.sub(r"https?://(?:www\.)?youtube\.com/embed/", "https://youtu.be/", url)
         return url
     return None
